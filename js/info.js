5901992e208f694bc871f9a0dd96073e58f6a84f
Bruno Rossetto <bruno.rossetto97@hotmail.com>
2016-10-24 20:13:58 -0200

    correção de imagens

--- js/info.js
+++ js/info.js
@@ -17,7 +17,7 @@
 		            add_resultados_tabela = add_resultados_tabela.replace("[[NOME]]", nome_person);
 		            $("#tabela_search_body").append(add_resultados_tabela);
 		        })
-		    
+
 		        // Se a tiver mais de uma página de personagens na pesquisa, pega o conteúdo do "next" e continua a recolher os dados da próxima página, até o "next" for nulo.
 		    if (data.next != null) {
 		        var next = data.next;
@@ -54,7 +54,7 @@
 		                completa_tabela = "<tr> <td>" + item.height + "</td> <td>" + item.mass + "</td> <td>" + item.gender + "</td> <td>" + valor.name + "</td></tr>";
 		                $("#tabela_info").html(completa_tabela);
 		                $("#nome").html("<i class='fa fa-info-circle'></i> " + item.name);
-		                imagens(personagem);
+		                $("#imagem").html(imagens(personagem));
 		            })
 		        })
 		    })
@@ -557,7 +557,7 @@
 		        }
 		        var nome = removerAcentos(nome);
 		        var nome_person = "'" + nome.toUpperCase() + "'";
-		        var conteudo_forca = '<br><br><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome_tamanho + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" type="text" onkeyup="verifica([[NOME]])" style="width:200px;" name="txtLetra" placeholder=" Letra..."></h2><p style="float:right; margin-right:50px; margin-top:90px;"><img src="img/yoda_result.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
+		        var conteudo_forca = '<br><br><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome_tamanho + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" type="text" onkeyup="verifica([[NOME]])" style="width:200px;" name="txtLetra" placeholder=" Letra..."></h2><p style="float:right; margin-right:50px; margin-top:90px;"><img src="img/starwars.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
 		        conteudo_forca = conteudo_forca.replace("[[NOME]]", nome_person);
 		        $("#form_forca").html(conteudo_forca);
 		        document.form_forca.txtLetra.focus(); // Deixa o input sempre selecionado
@@ -607,7 +607,7 @@
 		            errou = 2;
 		            count++;
 		            var nome_person = "'" + nome.toUpperCase() + "'";
-		            var conteudo_forca = '<br><br><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome_tamanho + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" type="text" style="width:200px;" onkeyup="verifica([[NOME]])" name="txtLetra" placeholder=" Letra..."></h2><p style="float:right; margin-right:50px; margin-top:90px;"><img src="img/yoda_result.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
+		            var conteudo_forca = '<br><br><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome_tamanho + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" type="text" style="width:200px;" onkeyup="verifica([[NOME]])" name="txtLetra" placeholder=" Letra..."></h2><p style="float:right; margin-right:50px; margin-top:90px;"><img src="img/starwars.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
 		            conteudo_forca = conteudo_forca.replace("[[NOME]]", nome_person);
 		            $("#form_forca").html(conteudo_forca);
 		            document.form_forca.txtLetra.focus();
@@ -629,7 +629,7 @@
 		        alert("Errou ):");
 		        qtd_erros += 1;
 		        var nome_person = "'" + nome.toUpperCase() + "'";
-		        var conteudo_forca = '<br><br><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome_tamanho + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" type="text" style="width:200px;" onkeyup="verifica([[NOME]])" name="txtLetra" placeholder=" Letra..."></h2><p style="float:right; margin-right:50px; margin-top:90px;"><img src="img/yoda_result.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
+		        var conteudo_forca = '<br><br><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome_tamanho + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" type="text" style="width:200px;" onkeyup="verifica([[NOME]])" name="txtLetra" placeholder=" Letra..."></h2><p style="float:right; margin-right:50px; margin-top:90px;"><img src="img/starwars.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
 		        conteudo_forca = conteudo_forca.replace("[[NOME]]", nome_person);
 		        $("#form_forca").html(conteudo_forca);
 		        document.form_forca.txtLetra.focus();
@@ -642,7 +642,7 @@
 		                if (nome_tamanho[n] == "__") {
 		                    conta_dica = 1;
 		                    var dica = n + 1;
-		                    var conteudo_forca = '<br><br><p>Dica: ' + dica + 'ª letra: ' + nome[n] + '</p><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome_tamanho + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" name="txtLetra" type="text" style="width:200px;" onkeyup="verifica([[NOME]])" placeholder=" Letra..."></h2><p style="float:right; margin-right:50px; margin-top:40px;"><img src="img/yoda_result.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
+		                    var conteudo_forca = '<br><br><p>Dica: ' + dica + 'ª letra: ' + nome[n] + '</p><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome_tamanho + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" name="txtLetra" type="text" style="width:200px;" onkeyup="verifica([[NOME]])" placeholder=" Letra..."></h2><p style="float:right; margin-right:50px; margin-top:40px;"><img src="img/starwars.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
 		                    conteudo_forca = conteudo_forca.replace("[[NOME]]", nome_person);
 		                    $("#form_forca").html(conteudo_forca);
 		                    document.form_forca.txtLetra.focus();
@@ -659,7 +659,7 @@
 		        alert("Perdeu ):");
 		        var nome_person = "'" + nome.toUpperCase() + "'";
 		        var conteudo_forca2 = '<p>O personagem era:</p>';
-		        var conteudo_forca = '<br><br><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" type="text" style="width:200px;" placeholder=" Letra..." disabled></h2><p style="float:right; margin-right:50px; margin-top:90px;"><img src="img/yoda_result.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
+		        var conteudo_forca = '<br><br><p style="margin-left:60px; margin-top:90px; width:10px; float:left;">' + nome + '</p><h2 style="float:left; margin-top:200px;"><input maxlength=1 id="inpt_forca" type="text" style="width:200px;" placeholder=" Letra..." disabled></h2><p style="float:right; margin-right:50px; margin-top:90px;"><img src="img/starwars.png"><br><span style="font-size:30px;">' + qtd_erros + '/6</span></p>';
 		        $("#form_forca").html(conteudo_forca);
 		        $("#form_forca").append(conteudo_forca2);
 		    }
@@ -812,149 +812,151 @@
 		// Exibe a imagem de acordo com o personagem selecionado na pesquisa.
 		function imagens(nome) {
 		    if (nome == "Yoda")
-		        $("#imagem").html('<img src="img/Yoda.ico" center>');
+		        return '<img src="img/Yoda.ico" center>';
 		    else if (nome == "Luke Skywalker")
-		        $("#imagem").html('<img src="img/skywalker.ico" center>');
+		       return('<img src="img/skywalker.ico" center>');
 		    else if (nome == "C-3PO")
-		        $("#imagem").html('<img src="img/C3PO.png" center>')
+		       return('<img src="img/C3PO.png" center>');
 		    else if (nome == "R2-D2")
-		        $("#imagem").html('<img src="img/r2-d2.png" center>')
+		        return('<img src="img/r2-d2.png" center>');
 		    else if (nome == "Darth Vader")
-		        $("#imagem").html('<img src="img/vader.ico" center>')
+		        return('<img src="img/vader.ico" center>');
 		    else if (nome == "Leia Organa")
-		        $("#imagem").html('<img src="img/Princess_Leia.png" center>')
+		       return('<img src="img/Princess_Leia.png" center>');
 		    else if (nome == "Ki-Adi-Mundi")
-		        $("#imagem").html('<img src="img/Ki-Adi-Mundi-icon.png" center>')
+		       return('<img src="img/Ki-Adi-Mundi-icon.png" center>');
 		    else if (nome == "Owen Lars")
-		        $("#imagem").html('<img src="img/Owen_Lars.png" center>')
+		      return('<img src="img/Owen_Lars.png" center>');
 		    else if (nome == "Beru Whitesun lars")
-		        $("#imagem").html('<img src="img/lars.png" center>')
+		      return('<img src="img/lars.png" center>');
 		    else if (nome == "R5-D4")
-		        $("#imagem").html('<img src="img/r5d4.png" center>')
+		       return('<img src="img/r5d4.png" center>');
 		    else if (nome == "Biggs Darklighter")
-		        $("#imagem").html('<img src="img/biggs.png" center>')
+		       return('<img src="img/biggs.png" center>');
 		    else if (nome == "Obi-Wan Kenobi")
-		        $("#imagem").html('<img src="img/Obi-wan-kenobi.png" center>')
+		       return('<img src="img/Obi-wan-kenobi.png" center>');
 		    else if (nome == "Anakin Skywalker")
-		        $("#imagem").html('<img src="img/anakin.png" center>')
+		        return('<img src="img/anakin.png" center>');
 		    else if (nome == "Wilhuff Tarkin")
-		        $("#imagem").html('<img src="img/Tarkin.png" center>')
+		       return('<img src="img/Tarkin.png" center>');
 		    else if (nome == "Chewbacca")
-		        $("#imagem").html('<img src="img/Chewbacca.ico" center>')
+		       return('<img src="img/Chewbacca.ico" center>');
 		    else if (nome == "Han Solo")
-		        $("#imagem").html('<img src="img/han.png" center>')
+		       return('<img src="img/han.png" center>');
 		    else if (nome == "Greedo")
-		        $("#imagem").html('<img src="img/Greedo.png" center>')
+		      return('<img src="img/Greedo.png" center>');
 		    else if (nome == "Jabba Desilijic Tiure")
-		        $("#imagem").html('<img src="img/Jabba.png" center>')
+		       return('<img src="img/Jabba.png" center>');
 		    else if (nome == "Wedge Antilles")
-		        $("#imagem").html('<img src="img/Hellmy.png" center>')
+		       return('<img src="img/Hellmy.png" center>');
 		    else if (nome == "Jek Tono Porkins")
-		        $("#imagem").html('<img src="img/jek.png" center>')
+		      return('<img src="img/jek.png" center>');
 		    else if (nome == "Palpatine")
-		        $("#imagem").html('<img src="img/palpa.ico" center>')
+		       return('<img src="img/palpa.ico" center>');
 		    else if (nome == "Nien Nunb")
-		        $("#imagem").html('<img src="img/nunb.png" center>')
+		       return('<img src="img/nunb.png" center>');
 		    else if (nome == "Wicket Systri Warrick")
-		        $("#imagem").html('<img src="img/Wicket.png" center>')
+		       return('<img src="img/Wicket.png" center>');
 		    else if (nome == "Mon Mothma")
-		        $("#imagem").html('<img src="img/mon.png" center>')
+		       return('<img src="img/mon.png" center>');
 		    else if (nome == "Ackbar")
-		        $("#imagem").html('<img src="img/Ackbar.png" center>')
+		      return('<img src="img/Ackbar.png" center>');
 		    else if (nome == "Lando Calrissian")
-		        $("#imagem").html('<img src="img/Lando.png" center>')
+		       return('<img src="img/Lando.png" center>');
 		    else if (nome == "Bossk")
-		        $("#imagem").html('<img src="img/bossk.png" center>')
+		        return('<img src="img/bossk.png" center>');
 		    else if (nome == "Boba Fett")
-		        $("#imagem").html('<img src="img/Boba-Fett-icon.png" center>')
+		       return('<img src="img/Boba-Fett-icon.png" center>');
 		    else if (nome == "Sebulba")
-		        $("#imagem").html('<img src="img/Sebulba.png" center>')
+		       return('<img src="img/Sebulba.png" center>');
 		    else if (nome == "Qui-Gon Jinn")
-		        $("#imagem").html('<img src="img/Qui.png" center>')
+		       return('<img src="img/Qui.png" center>');
 		    else if (nome == "Nute Gunray")
-		        $("#imagem").html('<img src="img/Nute.png" center>')
+		       return('<img src="img/Nute.png" center>');
 		    else if (nome == "Finis Valorum")
-		        $("#imagem").html('<img src="img/valorum.png" center>')
+		       return('<img src="img/valorum.png" center>');
 		    else if (nome == "Jar Jar Binks")
-		        $("#imagem").html('<img src="img/Jar.png" center>')
+		        return('<img src="img/Jar.png" center>');
 		    else if (nome == "Roos Tarpals")
-		        $("#imagem").html('<img src="img/tarpals.png" center>')
+		       return('<img src="img/tarpals.png" center>');
 		    else if (nome == "Rugor Nass")
-		        $("#imagem").html('<img src="img/nass.png" center>')
+		       return('<img src="img/nass.png" center>');
 		    else if (nome == "Ric Olié")
-		        $("#imagem").html('<img src="img/ric.png" center>')
+		       return('<img src="img/ric.png" center>');
 		    else if (nome == "Watto")
-		        $("#imagem").html('<img src="img/watto.png" center>')
+		       return('<img src="img/watto.png" center>');
 		    else if (nome == "Quarsh Panaka")
-		        $("#imagem").html('<img src="img/panaka.jpg" center>')
+		       return('<img src="img/panaka.jpg" center>');
 		    else if (nome == "Shmi Skywalker")
-		        $("#imagem").html('<img src="img/shmi.png" center>')
+		        return('<img src="img/shmi.png" center>');
 		    else if (nome == "Darth Maul")
-		        $("#imagem").html('<img src="img/maul.png" center>')
+		        return('<img src="img/maul.png" center>');
 		    else if (nome == "Bib Fortuna")
-		        $("#imagem").html('<img src="img/bib.png" center>')
+		       return('<img src="img/bib.png" center>');
 		    else if (nome == "Ayla Secura")
-		        $("#imagem").html('<img src="img/Aayla.png" center>')
+		       return('<img src="img/Aayla.png" center>');
 		    else if (nome == "Dud Bolt")
-		        $("#imagem").html('<img src="img/Dud.jpg" center>')
+		        return('<img src="img/Dud.jpg" center>');
 		    else if (nome == "Gasgano")
-		        $("#imagem").html('<img src="img/gasgano.png" center>')
+		        return('<img src="img/gasgano.png" center>');
 		    else if (nome == "Ben Quadinaros")
-		        $("#imagem").html('<img src="img/ben.png" center>')
+		        return('<img src="img/ben.png" center>');
 		    else if (nome == "Mace Windu")
-		        $("#imagem").html('<img src="img/windu.png" center>')
+		        return('<img src="img/windu.png" center>');
 		    else if (nome == "Kit Fisto")
-		        $("#imagem").html('<img src="img/fisto.png" center>')
+		       return('<img src="img/fisto.png" center>');
 		    else if (nome == "Eeth Koth")
-		        $("#imagem").html('<img src="img/koth.png" center>')
+		       return('<img src="img/koth.png" center>');
 		    else if (nome == "Adi Gallia")
-		        $("#imagem").html('<img src="img/galia.png" center>')
+		       return('<img src="img/galia.png" center>');
 		    else if (nome == "Saesee Tiin")
-		        $("#imagem").html('<img src="img/tiin.png" center>')
+		        return('<img src="img/tiin.png" center>');
 		    else if (nome == "Yarael Poof")
-		        $("#imagem").html('<img src="img/poof.jpg" center>')
+		        return('<img src="img/poof.jpg" center>');
 		    else if (nome == "Plo Koon")
-		        $("#imagem").html('<img src="img/koon.png" center>')
+		        return('<img src="img/koon.png" center>');
 		    else if (nome == "Mas Amedda")
-		        $("#imagem").html('<img src="img/Amedda.png" center>')
+		        return('<img src="img/Amedda.png" center>');
 		    else if (nome == "Gregar Typho")
-		        $("#imagem").html('<img src="img/typho.png" center>')
+		        return('<img src="img/typho.png" center>');
 		    else if (nome == "Cliegg Lars")
-		        $("#imagem").html('<img src="img/lars2.png" center>')
+		        return('<img src="img/lars2.png" center>');
 		    else if (nome == "Poggle the Lesser")
-		        $("#imagem").html('<img src="img/lesser.png" center>')
+		        return('<img src="img/lesser.png" center>');
 		    else if (nome == "Luminara Unduli")
-		        $("#imagem").html('<img src="img/luminara.jpg" center>')
+		       return('<img src="img/luminara.jpg" center>');
 		    else if (nome == "Barriss Offee")
-		        $("#imagem").html('<img src="img/barriss.png" center>')
+		       return('<img src="img/barriss.png" center>');
 		    else if (nome == "Dooku")
-		        $("#imagem").html('<img src="img/Dooku.png" center>')
+		        return('<img src="img/Dooku.png" center>');
 		    else if (nome == "Jango Fett")
-		        $("#imagem").html('<img src="img/fett.png" center>')
+		       return('<img src="img/fett.png" center>');
 		    else if (nome == "Dexter Jettster")
-		        $("#imagem").html('<img src="img/dexter.jpg" center>')
+		        return('<img src="img/dexter.jpg" center>');
 		    else if (nome == "Lama Su")
-		        $("#imagem").html('<img src="img/xx.png" center>')
+		        return('<img src="img/xx.png" center>');
 		    else if (nome == "Taun We")
-		        $("#imagem").html('<img src="img/xx.png" center>')
+		        return('<img src="img/xx.png" center>');
 		    else if (nome == "Jocasta Nu")
-		        $("#imagem").html('<img src="img/jocasta.png" center>')
+		        return('<img src="img/jocasta.png" center>');
 		    else if (nome == "Ratts Tyerell")
-		        $("#imagem").html('<img src="img/rats.png" center>')
+		        return('<img src="img/rats.png" center>');
 		    else if (nome == "R4-P17")
-		        $("#imagem").html('<img src="img/r2.jpg" center>')
+		        return('<img src="img/r2.jpg" center>');
 		    else if (nome == "Wat Tambor")
-		        $("#imagem").html('<img src="img/wat.png" center>')
+		        return('<img src="img/wat.png" center>');
 		    else if (nome == "San Hill")
-		        $("#imagem").html('<img src="img/hill.png" center>')
+		        return('<img src="img/hill.png" center>');
 		    else if (nome == "Shaak Ti")
-		        $("#imagem").html('<img src="img/shaak.png" center>')
+		        return('<img src="img/shaak.png" center>');
 		    else if (nome == "Tarfful")
-		        $("#imagem").html('<img src="img/Tarrful.png" center>')
+		       return('<img src="img/Tarrful.png" center>');
 		    else if (nome == "Tion Medon")
-		        $("#imagem").html('<img src="img/Pauan.png" center>')
+		       return('<img src="img/Pauan.png" center>');
 		    else if (nome == "BB8")
-		        $("#imagem").html('<img src="img/bb8.png" center>')
+		      return('<img src="img/bb8.png" center>');
 		    else if (nome == "Padmé Amidala")
-		        $("#imagem").html('<img src="img/padme.png" center>')
+		       return('<img src="img/padme.png" center>');
+		   else
+		   		return('<img src="img/default.png" center>');
 		}
